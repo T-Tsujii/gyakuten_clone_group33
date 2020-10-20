b8d927f5ab94edd3aa4b76b1864395bf3e70e228
Masanori Beppu <yaaman01@mbp-mb.local>
2020-10-20 23:07:39 +0900

    管理者画面_seeds.rb内の記述を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ADMIN＿EMAIL = 'admin@example.com'
+ADMIN＿PASSWORD = 'password'
+AdminUser.find_or_create_by!(email: ADMIN＿EMAIL) do |user|
+  user.password = ADMIN＿PASSWORD
+end
+
+
 EMAIL = 'test@example.com'
 PASSWORD = 'password'
-
 # テストユーザが存在しない時だけ作成
 User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
-end
-
-
-
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-
-EMAIL = 'admin@example.com'
-PASSWORD = 'password'
-AdminUser.find_or_create_by!(email: EMAIL) do |user|
-  user.password = PASSWORD
 end
