2f41d6af64c02d15d919d503a5eacbc7a943756d
Masanori Beppu <yaaman01@mbp-mb.local>
2020-10-21 12:06:04 +0900

    管理者画面の実装_texts.rbとmovies.rbのparmit_paramsを変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
 ADMIN＿EMAIL = 'admin@example.com'
 ADMIN＿PASSWORD = 'password'
-AdminUser.find_or_create_by!(email: ADMIN＿EMAIL) do |user|
-  user.password = ADMIN＿PASSWORD
-end
-
-
 EMAIL = 'test@example.com'
 PASSWORD = 'password'
+
+
 # テストユーザが存在しない時だけ作成
 User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
+end
+
+
+AdminUser.find_or_create_by!(email: ADMIN＿EMAIL) do |user|
+  user.password = ADMIN＿PASSWORD
 end
