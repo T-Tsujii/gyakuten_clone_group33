34054e42651dd0a2e069b2842273cbc5f5aec655
Masanori Beppu <yaaman01@mbp-mb.local>
2020-10-24 21:08:46 +0900

    bin/railsファイルを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   devise_for :users
   root "movies#index"
   resources :movies
-  resources :aws_texts,only:[:index,:show]
+  resources :aws_texts, only: [:index, :show]
 end
